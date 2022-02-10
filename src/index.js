d55a8d703dcb433d2f3a61466b35569462572151
Alex Makeyev <makeyev1987@gmail.com>
2022-02-10 22:32:33 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ const btnMinus = document.querySelector('button[data-minus]');
 const btnMult = document.querySelector('button[data-multiply]');
 const btnDiv = document.querySelector('button[data-divide]');
 const btnClear = document.querySelector('button[data-clear]');
-const valueInput = document.querySelector('input[data-value]');
+const valueInput1 = document.querySelector('input[data-value1]');
+const valueInput2 = document.querySelector('input[data-value2]');
 const outputEl = document.querySelector('span[data-result]');
 let total = 0;
 btnPlus.addEventListener('click', function () {
-    const value = Number(valueInput.value);
-    console.log(value);
-    total += value;
+    const value1 = Number(valueInput1.value);
+    const value2 = Number(valueInput2.value);
+    total = value1 + value2;
     outputEl.textContent = total
-    valueInput.value = '';
+    valueInput1.value = '';
+    valueInput2.value = '';
 
 })
 btnClear.addEventListener('click', function () {
@@ -19,26 +21,26 @@ btnClear.addEventListener('click', function () {
     total = 0;
 })
 btnMinus.addEventListener('click', function () {
-    const value = Number(valueInput.value);
-    console.log(value);
-    total -= value;
+    const value1 = Number(valueInput1.value);
+    const value2 = Number(valueInput2.value);
+    total = value1 - value2;
     outputEl.textContent = total
-    valueInput.value = '';
-
+    valueInput1.value = '';
+    valueInput2.value = '';
 })
 btnMult.addEventListener('click', function () {
-    const value = Number(valueInput.value);
-    console.log(value);
-    total = total * value;
+    const value1 = Number(valueInput1.value);
+    const value2 = Number(valueInput2.value);
+    total = value1 * value2;
     outputEl.textContent = total
-    valueInput.value = '';
-
+    valueInput1.value = '';
+    valueInput2.value = '';
 })
 btnDiv.addEventListener('click', function () {
-    const value = Number(valueInput.value);
-    console.log(value);
-    total = total / value;
+    const value1 = Number(valueInput1.value);
+    const value2 = Number(valueInput2.value);
+    total = value1 / value2;
     outputEl.textContent = total
-    valueInput.value = '';
-
+    valueInput1.value = '';
+    valueInput2.value = '';
 })
